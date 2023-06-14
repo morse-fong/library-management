6db5868737b438b91c689db9b89cb500d6db9cbc
morse_fong <morse_fong@163.com>
2023-06-14 20:18:32 +0800

    邮箱测试

--- library-management-system-hou/config.js
+++ library-management-system-hou/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     email: {//邮件配置
-        service: 'morse_fong@163.com', // 邮箱
-        user: 'morse_fong@163.com', // 邮箱
-        pass: 'HUWOQSPRHVHZRITU', //授权码
+        service: 'library_management@163.com', // 邮箱
+        user: 'library_management@163.com', // 邮箱
+        pass: 'IATHBGPBNDZAQLOW', //授权码
     }
 }
